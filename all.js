baece456572ec7bd130bdb2b3c0f009209615a3c
chunhsianglan <nomo.webdesign@gmail.com>
2022-12-07 22:25:22 +0000

    update

---
+++ all.js
@@ -0,0 +1,10 @@
+// 1. TodoList: 可新增和刪除待辦事項。
+// 2. TodoList: 待辦事項會有狀態（完成與否），可透過 checkbox 來切換。
+// 3. TodoList: 待辦清單列表會有『全部』、『完成』、『未完成』Tab 來做篩選切換。
+// 4. TodoList: 可以清除全部已完成功能。
+// 5. TodoList: 會顯示出待完成的待辦有幾項。
+
+
+const addText = document.querySelector('.addText');
+const btn_add = document.querySelector('.btn_add');
+console.log(addText,btn_add)
